55581fe74e9e546764850551d88744aa3be58c90
robitobi01 <robitobi01@gmx.de>
2021-11-07 14:24:30 +0100

    added region indicator and copy to clipboard function

--- src/data/template/static/js/control/mousepos.js
+++ src/data/template/static/js/control/mousepos.js
@@ -7,26 +7,51 @@ function MousePosControl() {
 }
 
 MousePosControl.prototype.create = function(wrapper) {
+    var message = document.createElement("p");
+    message.setAttribute("id", "message-box");
+    message.setAttribute("class", "btn");
+    message.style.display = "none";
+    message.innerHTML ="Text copied!"
+	wrapper.appendChild(message);
+
 	var text = document.createElement("span");
 	text.setAttribute("id", "mouse-move-div");
+    text.innerHTML = '<div class="btn-group" role="group">'
+			+ '<button type="button" class="btn btn-default" id="btn-x" onclick="MousePosControl.prototype.copyToClipboard(\'btn-x\')"></button>'
+			+ '<button type="button" class="btn btn-default" id="btn-z" onclick="MousePosControl.prototype.copyToClipboard(\'btn-z\')"></button>'
+            + '<button type="button" class="btn btn-default" id="btn-mca" onclick="MousePosControl.prototype.copyToClipboard(\'btn-mca\')"></button>'
+			+ "</div>";
 
 	var updatePos = function(ui) {
 		return function(event) {
 			var xzy = ui.latLngToMC(event.latlng, 64);
-			document.getElementById("mouse-move-div").innerHTML = '<div class="btn-group" role="group">'
-			+ '<button type="button" class="btn btn-default">' + "X: " + Math.round(xzy[0]) + '</button>'
-			+ '<button type="button" class="btn btn-default">' + "Z: " + Math.round(xzy[1]) + '</button>'
-			+ '<button type="button" class="btn btn-default">' + "Y: " + Math.round(xzy[2]) + '</button>'
-			+ "</div>";
-		};
+            document.getElementById("btn-x").innerHTML = 'X: ' + Math.round(xzy[0])
+            document.getElementById("btn-z").innerHTML = 'Z: ' + Math.round(xzy[1])
+            document.getElementById("btn-mca").innerHTML = 'r.' + (xzy[0] >> 9) + '.' + (xzy[1] >> 9) + '.mca'
+        };
 	}(this.ui);
-
 	this.ui.lmap.on("mousemove", updatePos);
 	this.ui.lmap.on("mousedown", updatePos);
-
 	wrapper.appendChild(text);
 };
 
 MousePosControl.prototype.getName = function() {
 	return 'mouse-pos';
+};
+
+MousePosControl.prototype.copyToClipboard = function(id) {
+    var text = document.createElement('textarea');
+    var value = document.getElementById(id).innerHTML;
+    if (value.includes(' ')) {
+        value = value.split(' ', 2)[1];
+    }
+    text.value = value;
+    document.body.appendChild(text);
+    text.select();
+    document.execCommand('copy');
+    document.body.removeChild(text);
+    var box = document.getElementById('message-box')
+    box.innerHTML = "Copied: " + value;
+    box.style.display = "block";
+    setTimeout(function(){box.style.display = "none";}, 1000);
 };
